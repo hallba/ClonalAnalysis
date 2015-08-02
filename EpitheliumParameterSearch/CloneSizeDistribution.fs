23224121ea24299686eeb777faf15982c882d29e
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-08-02 19:20:30 +0100

    Included some of the original tests

--- EpitheliumParameterSearch/CloneSizeDistribution.fs
+++ EpitheliumParameterSearch/CloneSizeDistribution.fs
@@ -19,51 +19,17 @@ let F x y t r gamma =
     let u = ((complex 1. 0.)-y)*exp(complex (-gamma*t) 0.) 
     let u0 = ((complex 1. 0.)-y )
     
-    //printf "x %A t %A r %A gamma %A\n" x t r gamma
-    printf "v %A w %A gg %A u %A u0 %A\n" v w gg u u0
+    //printf "v %A w %A gg %A u %A u0 %A\n" v w gg u u0
 
     let Q = (complex (1. + 2.*w) 0.) - gg*u0 + ( (complex (2.*r) 0.)*(x-y) + y - (complex 1. 0.) )/(complex gamma 0.)
     
-    //REALLY interesting bug. Forget to put the brackets around the  gg*u0- type checker does not find this, gives different result
+    //Interesting bug. Forget to put the brackets around the  gg*u0- type checker does not find this, gives different result
     let C = ((-Q*(Whittaker.M w 0. (gg*u0)) ) + (complex (1.+2.*w) 0.)*(Whittaker.M (1.+w) 0. (gg*u0))) / (Q*(Whittaker.W w 0. (gg*u0) )+(complex 2. 0.)*(Whittaker.W (1.+w) 0. (gg*u0) ) )
-    //
-//Matlab
-//    C = (-Q*whittakerM(w,0,gg*u0) + (1+2*w)*whittakerM(1+w,0,gg*u0)) / ...
-//    (Q * whittakerW(w,0,gg*u0) + 2*whittakerW(1+w,0,gg*u0))
-    printf "Q %A C %A" Q C
-    //Examine C in depth
+
     (complex 1. 0.) - u + 
     (u*(complex (1.+v) 0.)- (complex (gamma*(1.+2.*w)) 0.) ) / complex (2.*r) 0. +
     (complex (gamma/(2.*r)) 0.) * ( (complex (1.+2.*w) 0.)* (Whittaker.M (1.+w) 0. (u*gg) ) - (complex 2. 0.)*C*(Whittaker.W (1.+w) 0. (u*gg) ) ) /
     ( (Whittaker.M w 0. (u*gg)) + C* (Whittaker.W w 0. (u*gg)) )    
-
-let G z t r gamma =
-    List.map (fun zMember -> if zMember = complex 1. 0. then complex 1. 0. else F zMember zMember t r gamma) z
-//    let rec core z t r gamma acc =
-//        match z with
-//        | [] -> List.rev acc
-//        | topZ::rest -> if topZ = complex 1. 0. then core z t r gamma ((complex 1. 0.)::acc) else core z t r gamma ((F topZ topZ t r gamma)::acc)
-//    core z t r gamma []
-
-//function g = G(z,t,r,gamma)
-//
-//for k=1:length(z)
-//    if(z(k)==1)
-//        g(k) = 1;
-//    else
-//    
-//        g(k) = F(z(k),z(k),t,r,gamma);
-//        
-//    end
-//
-//end
-//end
-
-//function z = u(y,t)
-//
-//z = (1-y)*exp(-t);
-//
-//end
 
 type parameterSet = {   time: float<week>;
                         rho: float; 
@@ -71,11 +37,10 @@ type parameterSet = {   time: float<week>;
                         lambda: float<cell/week>
                         } with 
                         member this.migration = this.rho/(1.-this.rho) * this.lambda
+                        member this.correctPathologicalPoint = { this with rho = this.rho + 0.0001 ; r = this.r+0.00001<probability> }
+                        override this.ToString() = sprintf "Rho: %A r:%A Time:%A Lambda:%A" this.rho this.r this.time this.lambda
 
 let testSystem = {time=1.<week>; rho=0.85; r=0.15<probability>; lambda=2.<cell/week>}
-
-let correctPathologicalPoints inputParameters =
-    { inputParameters with rho = inputParameters.rho + 0.00001 }
 
 let probabilityClonalSurvival inputParameters =
     //Note that the below line is incorrect, and the effects corrected later
@@ -93,7 +58,7 @@ let complexSum c =
         | [] -> acc
     core c (complex 0. 0.)
     
-let probabilityCloneSizes inputParameters nRange maxN =
+let rec probabilityCloneSizes inputParameters nRange maxN =
     let N = if List.max nRange >= maxN then List.max nRange + 1 else maxN
     let gamma = 1./inputParameters.rho - 1.
     let T = inputParameters.lambda * inputParameters.time
@@ -101,5 +66,12 @@ let probabilityCloneSizes inputParameters nRange maxN =
     let gVals = List.map (fun zMember -> if zMember = complex 1. 0. then complex 1. 0. else F zMember zMember (T*1.<cell^-1>) (inputParameters.r*1.<probability^-1>) gamma) k 
     let p = List.map (fun n -> List.mapi (fun index i -> i * exp(complex 0. (-2.*System.Math.PI*float(n)*float(index)/float(N)) ) ) gVals ) nRange 
             |>List.map (fun item -> (complexSum item).r/float(N) )
-    p
+            |>List.map (fun item -> if item < 0. then 0. else item) //In the matlab code we had an additional test which could allow values less
+                                                                    //than -1e-5 to be included in the output. Perhaps include in future?
+    let totalP = List.sum p
+    //Test for pathological points
+    if totalP > 1. || totalP < 0. || System.Double.IsNaN(totalP) then 
+        //NB: this is from the matlab code- note that it is a specified small perturbation, not 0.1%
+        ignore (printf "Pathological point: %A. Making 0.1%% perturbation" inputParameters); probabilityCloneSizes inputParameters.correctPathologicalPoint nRange maxN else
+        p
 
