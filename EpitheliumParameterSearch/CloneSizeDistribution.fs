e5fcc9739babfbc427002564b921b28cdc782f74
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-08-03 18:22:40 +0100

    Parameter scan functions

--- EpitheliumParameterSearch/CloneSizeDistribution.fs
+++ EpitheliumParameterSearch/CloneSizeDistribution.fs
@@ -57,21 +57,65 @@ let complexSum c =
         | top::rest -> core rest (acc+top)
         | [] -> acc
     core c (complex 0. 0.)
-    
-let rec probabilityCloneSizes inputParameters nRange maxN =
-    let N = if List.max nRange >= maxN then List.max nRange + 1 else maxN
-    let gamma = 1./inputParameters.rho - 1.
-    let T = inputParameters.lambda * inputParameters.time
-    let k = List.init N (fun item -> exp(complex 0. (float(item)/float(N)*System.Math.PI*2.)) )
-    let gVals = List.map (fun zMember -> if zMember = complex 1. 0. then complex 1. 0. else F zMember zMember (T*1.<cell^-1>) (inputParameters.r*1.<probability^-1>) gamma) k 
-    let p = List.map (fun n -> List.mapi (fun index i -> i * exp(complex 0. (-2.*System.Math.PI*float(n)*float(index)/float(N)) ) ) gVals ) nRange 
-            |>List.map (fun item -> (complexSum item).r/float(N) )
-            |>List.map (fun item -> if item < 0. then 0. else item) //In the matlab code we had an additional test which could allow values less
-                                                                    //than -1e-5 to be included in the output. Perhaps include in future?
-    let totalP = List.sum p
-    //Test for pathological points
-    if totalP > 1. || totalP < 0. || System.Double.IsNaN(totalP) then 
-        //NB: this is from the matlab code- note that it is a specified small perturbation, not 0.1%
-        ignore (printf "Pathological point: %A. Making 0.1%% perturbation" inputParameters); probabilityCloneSizes inputParameters.correctPathologicalPoint nRange maxN else
-        p
 
+let probabilityCloneSizes inputParameters nRange maxN =
+    let rec core inputParameters nRange maxN attempt maxAttempts =
+        //No infinite loops
+        if attempt = maxAttempts then failwith("Too many attempts to modify a pathological point")
+        
+        let N = if List.max nRange >= maxN then List.max nRange + 1 else maxN
+        let gamma = 1./inputParameters.rho - 1.
+        let T = inputParameters.lambda * inputParameters.time
+        let k = List.init N (fun item -> exp(complex 0. (float(item)/float(N)*System.Math.PI*2.)) )
+        let gVals = List.map (fun zMember -> if zMember = complex 1. 0. then complex 1. 0. else F zMember zMember (T*1.<cell^-1>) (inputParameters.r*1.<probability^-1>) gamma) k 
+        let p = List.map (fun n -> List.mapi (fun index i -> i * exp(complex 0. (-2.*System.Math.PI*float(n)*float(index)/float(N)) ) ) gVals ) nRange 
+                |>List.map (fun item -> (complexSum item).r/float(N) )
+                |>List.map (fun item -> if item < 0. then 0. else item) //In the matlab code we had an additional test which could allow values less
+                                                                        //than -1e-5 to be included in the output. Perhaps include in future?
+        let totalP = List.sum p
+        //Test for pathological points
+        if totalP > 1. || totalP < 0. || System.Double.IsNaN(totalP) then 
+            //NB: this is from the matlab code- note that it is a specified small perturbation, not 0.1%
+            ignore (printf "Pathological point: %A. Making 0.1%% perturbation" inputParameters); core inputParameters.correctPathologicalPoint nRange maxN (attempt+1) maxAttempts else
+            p |> Array.ofList
+    core inputParameters nRange maxN 0 100
+
+type parameterSearch = {    rhoRange:       float array
+                            rRange:         float<probability> array
+                            lambdaRange:    float<cell/week> array
+                            timePoints:     float<week> array
+                            maxN:           int
+                            cloneSizeMatrix:  float [] [] [] [] []// lambda x rho x r x time x n
+                            survivalMatrix: MathNet.Numerics.complex [] [] [] [] //lambda x rho x r x time
+                            }
+
+let createParameterSet rhoRange rRange lambdaRange timePoints = 
+    [ for lambda in lambdaRange do for rho in rhoRange do for r in rRange do for t in timePoints do yield {testSystem with lambda=lambda;time=t;r=r;rho=rho} ]
+    |> Array.ofList
+
+let restructureParameterSet rhoRange rRange lambdaRange timePoints maxN (oneDimensionalSurvival:MathNet.Numerics.complex []) (oneDimensionalCloneSize: float [] []) =
+    let rhoN = Array.length rhoRange
+    let rN = Array.length rRange
+    let lambdaN = Array.length lambdaRange
+    let timePointsN = Array.length timePoints
+    //Need to check the order of the matrix to avoid matrix transposition
+    let probS = Array.init lambdaN (fun lambda -> Array.init rhoN (fun rho -> Array.init rN (fun r -> Array.init timePointsN (fun t -> oneDimensionalSurvival.[lambda*lambdaN*rhoN*rN+rho*rN*rhoN+r*rN+t]))))
+    let probN = Array.init lambdaN (fun lambda -> Array.init rhoN (fun rho -> Array.init rN (fun r -> Array.init timePointsN (fun t -> oneDimensionalCloneSize.[lambda*lambdaN*rhoN*rN+rho*rN*rhoN+r*rN+t]))))
+    {   rhoRange    =   rhoRange
+        rRange      =   rRange
+        lambdaRange =   lambdaRange
+        timePoints  =   timePoints
+        maxN        =   maxN
+        cloneSizeMatrix =   probN
+        survivalMatrix  =   probS
+        }
+
+let parameterSearch rhoRange rRange lambdaRange timePoints maxN =
+    let completeSet = createParameterSet rhoRange rRange lambdaRange timePoints
+    
+    //Get numbers
+    let probS = Array.Parallel.map (fun input -> probabilityClonalSurvival input) completeSet
+    let probN = Array.Parallel.map (fun input -> probabilityCloneSizes input (List.init maxN (fun i -> i+1)) maxN) completeSet
+
+    //Restructure results and put into a record
+    restructureParameterSet rhoRange rRange lambdaRange timePoints maxN probS probN
