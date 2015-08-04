a53d910c9c9b9ef7be69e188533a5321a8fe928f
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-08-04 19:15:27 +0100

    Fixed the parameter sweep

--- EpitheliumParameterSearch/CloneSizeDistribution.fs
+++ EpitheliumParameterSearch/CloneSizeDistribution.fs
@@ -103,7 +103,7 @@ type parameterSearch = {    rhoRange:       float array
                             timePoints:     float<week> array
                             maxN:           int
                             cloneSizeMatrix:  float [] [] [] [] []// lambda x rho x r x time x n
-                            survivalMatrix: MathNet.Numerics.complex [] [] [] [] //lambda x rho x r x time
+                            survivalMatrix: float [] [] [] [] //lambda x rho x r x time
                             }
 
 let createParameterSet rhoRange rRange lambdaRange timePoints = 
@@ -116,8 +116,9 @@ let restructureParameterSet rhoRange rRange lambdaRange timePoints maxN (oneDime
     let lambdaN = Array.length lambdaRange
     let timePointsN = Array.length timePoints
     //Need to check the order of the matrix to avoid matrix transposition
-    let probS = Array.init lambdaN (fun lambda -> Array.init rhoN (fun rho -> Array.init rN (fun r -> Array.init timePointsN (fun t -> oneDimensionalSurvival.[lambda*lambdaN*rhoN*rN+rho*rN*rhoN+r*rN+t] ))))
-    let probN = Array.init lambdaN (fun lambda -> Array.init rhoN (fun rho -> Array.init rN (fun r -> Array.init timePointsN (fun t -> oneDimensionalCloneSize.[lambda*lambdaN*rhoN*rN+rho*rN*rhoN+r*rN+t]))))
+    printf "Length %A -> Expected %A\n" (Array.length oneDimensionalSurvival) (rhoN*rN*lambdaN*timePointsN)
+    let probS = Array.init lambdaN (fun lambda -> Array.init rhoN (fun rho -> Array.init rN (fun r -> Array.init timePointsN (fun t -> oneDimensionalSurvival.[t+(r*timePointsN)+(rho*timePointsN*rN)+(lambda*rhoN*timePointsN*rN)].r  ))))
+    let probN = Array.init lambdaN (fun lambda -> Array.init rhoN (fun rho -> Array.init rN (fun r -> Array.init timePointsN (fun t -> oneDimensionalCloneSize.[t+(r*timePointsN)+(rho*timePointsN*rN)+(lambda*rhoN*timePointsN*rN)] ))))
     {   rhoRange    =   rhoRange
         rRange      =   rRange
         lambdaRange =   lambdaRange
