1cc6386f5b3d01b1b9b0f2c1dce67f525d562424
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-08-05 10:45:36 +0100

    Added more reporting to find sources of error

--- EpitheliumParameterSearch/Kummer.fs
+++ EpitheliumParameterSearch/Kummer.fs
@@ -9,24 +9,36 @@ let debug = true
 let gamma = MathNet.Numerics.SpecialFunctions.Gamma
 
 let rec complexGamma (z:System.Numerics.Complex) =
-    //Lanczos approximation, based on wikipedia implementation
-    //http://creativecommons.org/licenses/by-sa/3.0/
-    let p = [   complex 676.5203681218851 0.;
-                complex -1259.1392167224028 0.;
-                complex 771.32342877765313 0.;
-                complex -176.61502916214059 0.;
-                complex 12.507343278686905 0.;
-                complex -0.13857109526572012 0.;
-                complex 9.9843695780195716e-6 0.;
-                complex 1.5056327351493116e-7 0.  ]
-    let complexPi = complex System.Math.PI 0.
-    if z.r < 0.5 then complexPi / ( (Trig.Sin (complexPi*z) ) * complexGamma ( (complex 1. 0.) - z) )
-        else    let z = z - complex 1. 0.
-                //let x = complex 0.99999999999980993 0.
-                let x  = List.mapi (fun i pVal -> pVal/(z+complex (float(i)+1.) 0.)) p
-                        |> List.fold (fun acc pVal -> acc+pVal ) (complex 0.99999999999980993 0.)  
-                let t = (complex (float(List.length p) - 0.5) 0.) + z
-                sqrt( (complex 2. 0.)*complexPi) * t**(z+(complex 0.5 0.)) * exp(-t) * x
+    let lanczos (z:complex) =
+        //Lanczos approximation, based on wikipedia implementation
+        //http://creativecommons.org/licenses/by-sa/3.0/
+        let p = [   complex 676.5203681218851 0.;
+                    complex -1259.1392167224028 0.;
+                    complex 771.32342877765313 0.;
+                    complex -176.61502916214059 0.;
+                    complex 12.507343278686905 0.;
+                    complex -0.13857109526572012 0.;
+                    complex 9.9843695780195716e-6 0.;
+                    complex 1.5056327351493116e-7 0.  ]
+        let complexPi = complex System.Math.PI 0.
+        if z.r < 0.5 then complexPi / ( (Trig.Sin (complexPi*z) ) * complexGamma ( (complex 1. 0.) - z) )
+            else    let z = z - complex 1. 0.
+                    //let x = complex 0.99999999999980993 0.
+                    let x  = List.mapi (fun i pVal -> pVal/(z+complex (float(i)+1.) 0.)) p
+                            |> List.fold (fun acc pVal -> acc+pVal ) (complex 0.99999999999980993 0.)  
+                    let t = (complex (float(List.length p) - 0.5) 0.) + z
+                    let result = sqrt( (complex 2. 0.)*complexPi) * t**(z+(complex 0.5 0.)) * exp(-t) * x
+                    if debug then printf "complex gamma: z=%A result=%A\ngamma(%A+%Ai)\n" z result z.r z.i
+                    result
+    //gamma needs to be complex but lanczos algorithm as implemented is weak for small real z
+    //Test and fallback to Numerics gamma if safe to do so
+    //lanczos might work better if the function was implemented as a series computing to desired precision
+    if z.i = 0. 
+        then    let result = complex (gamma(z.r)) 0. 
+                if debug then printf "Real gamma: z=%A result=%A\ngamma(%A)\n" z.r result z.r
+                result
+        else lanczos z
+
 
 let factorial n =
     let rec core n acc =
--- EpitheliumParameterSearch/Whittaker.fs
+++ EpitheliumParameterSearch/Whittaker.fs
@@ -6,10 +6,14 @@ let M (k:complex) m (z:complex) =
     //let constant =  z**(complex (m+0.5) 0. ) * exp(-z/(complex 2. 0.)) 
     let constant =  z**( m + (complex 0.5 0.) ) * exp(-z/(complex 2. 0.)) 
     if System.Double.IsNaN(k.Magnitude) || System.Double.IsNaN(m.Magnitude) || System.Double.IsNaN(z.Magnitude)  then failwith "Whittaker M NaN input: a %A b %A z %A\n" m k z
-    constant * (Kummer.M (m - k + (complex 0.5 0.) ) ( (complex 1. 0.) + (complex 2. 0.)*m) z) 
-
+    let result = constant * (Kummer.M (m - k + (complex 0.5 0.) ) ( (complex 1. 0.) + (complex 2. 0.)*m) z) 
+    if Kummer.debug then printf "WhittakerM k=%A m=%A z=%A result=%A\nwhittakerW(%A+%Ai,%A+%Ai,%A+%Ai)\n" k m z result k.r k.i m.r m.i z.r z.i
+    result
+    
 let W (k:complex) m (z:complex) =
     let constant =  z**(m + complex 0.5 0. ) * exp(-z/(complex 2. 0.)) 
     if System.Double.IsNaN(k.Magnitude) || System.Double.IsNaN(m.Magnitude) || System.Double.IsNaN(z.Magnitude) then failwith "Whittaker W NaN input: a %A b %A z %A\n" m k z
-    constant * (Kummer.U (m - k + (complex 0.5 0.) ) ( (complex 1. 0.) + (complex 2. 0.)*m) z) 
+    let result = constant * (Kummer.U (m - k + (complex 0.5 0.) ) ( (complex 1. 0.) + (complex 2. 0.)*m) z) 
+    if Kummer.debug then printf "WhittakerW k=%A m=%A z=%A result=%A\nwhittakerW(%A+%Ai,%A+%Ai,%A+%Ai)\n" k m z result k.r k.i m.r m.i z.r z.i
+    result
     //MathNet.Numerics.SpecialFunctions.Gamma(-2.*m) * (M k m z) / MathNet.Numerics.SpecialFunctions.Gamma(0.5 - k - m) + MathNet.Numerics.SpecialFunctions.Gamma(2.*m) * (M k -m z) / MathNet.Numerics.SpecialFunctions.Gamma(0.5 - k + m)
