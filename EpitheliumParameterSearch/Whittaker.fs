aaaadb614c060c28d60736b8ed14244a11b2c359
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-08-05 10:47:43 +0100

    Typo in debug message

--- EpitheliumParameterSearch/Whittaker.fs
+++ EpitheliumParameterSearch/Whittaker.fs
@@ -7,7 +7,7 @@ let M (k:complex) m (z:complex) =
     let constant =  z**( m + (complex 0.5 0.) ) * exp(-z/(complex 2. 0.)) 
     if System.Double.IsNaN(k.Magnitude) || System.Double.IsNaN(m.Magnitude) || System.Double.IsNaN(z.Magnitude)  then failwith "Whittaker M NaN input: a %A b %A z %A\n" m k z
     let result = constant * (Kummer.M (m - k + (complex 0.5 0.) ) ( (complex 1. 0.) + (complex 2. 0.)*m) z) 
-    if Kummer.debug then printf "WhittakerM k=%A m=%A z=%A result=%A\nwhittakerW(%A+%Ai,%A+%Ai,%A+%Ai)\n" k m z result k.r k.i m.r m.i z.r z.i
+    if Kummer.debug then printf "WhittakerM k=%A m=%A z=%A result=%A\nwhittakerM(%A+%Ai,%A+%Ai,%A+%Ai)\n" k m z result k.r k.i m.r m.i z.r z.i
     result
     
 let W (k:complex) m (z:complex) =
