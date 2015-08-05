1b40b97ac3868aea21afeae173077643ded17f02
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-08-05 19:06:42 +0100

    Updated with a version of Stirling formula for gamma function Still needs refinement/replacement and testing

--- EpitheliumParameterSearch/Kummer.fs
+++ EpitheliumParameterSearch/Kummer.fs
@@ -9,6 +9,10 @@ let debug = true
 let gamma = MathNet.Numerics.SpecialFunctions.Gamma
 
 let rec complexGamma (z:System.Numerics.Complex) =
+    let complexPi = complex System.Math.PI 0.
+    let reflection z =
+        let oneMinusZ = complex 1. 0. - z
+        complexPi / ( (Trig.Sin (complexPi*z) ) * complexGamma ( oneMinusZ ) )       
     let lanczos (z:complex) =
         //Lanczos approximation, based on wikipedia implementation
         //http://creativecommons.org/licenses/by-sa/3.0/
@@ -20,24 +24,40 @@ let rec complexGamma (z:System.Numerics.Complex) =
                     complex -0.13857109526572012 0.;
                     complex 9.9843695780195716e-6 0.;
                     complex 1.5056327351493116e-7 0.  ]
-        let complexPi = complex System.Math.PI 0.
-        if z.r < 0.5 then complexPi / ( (Trig.Sin (complexPi*z) ) * complexGamma ( (complex 1. 0.) - z) )
-            else    let z = z - complex 1. 0.
+        let z' = z - complex 1. 0.
                     //let x = complex 0.99999999999980993 0.
-                    let x  = List.mapi (fun i pVal -> pVal/(z+complex (float(i)+1.) 0.)) p
+        let x  = List.mapi (fun i pVal -> pVal/(z'+complex (float(i)+1.) 0.)) p
                             |> List.fold (fun acc pVal -> acc+pVal ) (complex 0.99999999999980993 0.)  
-                    let t = (complex (float(List.length p) - 0.5) 0.) + z
-                    let result = sqrt( (complex 2. 0.)*complexPi) * t**(z+(complex 0.5 0.)) * exp(-t) * x
-                    if debug then printf "complex gamma: z=%A result=%A\ngamma(%A+%Ai)\n" z result z.r z.i
-                    result
-    //gamma needs to be complex but lanczos algorithm as implemented is weak for small real z
-    //Test and fallback to Numerics gamma if safe to do so
-    //lanczos might work better if the function was implemented as a series computing to desired precision
+        let t = (complex (float(List.length p) - 0.5) 0.) + z'
+        let result = sqrt( (complex 2. 0.)*complexPi) * t**(z'+(complex 0.5 0.)) * exp(-t) * x
+        result
+    let logGamma (z:complex) =
+        //Stirlings formula
+        let coefficients = [    complex (1./12.) 0.;
+                                complex (-1./360.) 0.;
+                                complex (1./1260.) 0.;
+                                complex (-1./1680.) 0.;
+                                complex (1./1188.) 0.;
+                                complex (-691./360360.) 0.;
+                                complex (1./156.) 0.;
+                                complex (-3617./122400.) 0.;
+                                complex (43867./244188.) 0.;
+                                complex -1.39243221690590 0.  
+                                ]
+        let result = List.mapi (fun i c -> c / (z**(complex (float(i*2+1)) 0. )) ) coefficients
+                     |> List.fold (fun acc item -> acc + item) ( (z - (complex 0.5 0.))*log(z) - z + (complex (0.5*log(2.*System.Math.PI)) 0.) )
+        result
+    //gamma needs to be complex but Stirlings/Lanczos are less precise approximations
+    //Better to keep to one function or mix/match? Stirling gives NaN for too many real z but lanczos is too imprecise
     if z.i = 0. 
         then    let result = complex (gamma(z.r)) 0. 
                 if debug then printf "Real gamma: z=%A result=%A\ngamma(%A)\n" z.r result z.r
                 result
-        else lanczos z
+        elif z.r < 0.5 then reflection z
+        else
+                let result = exp(logGamma z) //lanczos z
+                if debug then printf "Complex gamma: z=%A result=%A\ncgama(%A,%A,1)\n" z result z.r z.i
+                result
 
 
 let factorial n =
