419276bba2493c8f36a14dfe335e3bc9ece0fb16
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-10-20 23:08:46 +0100

    Initial simulator

--- EpitheliumParameterSearch/SimulationCloneSizeDistribution.fs
+++ EpitheliumParameterSearch/SimulationCloneSizeDistribution.fs
@@ -1,14 +1,11 @@
 ﻿module SimulationCloneSizeDistribution
 
-type division = {   AA  :   float;
-                    BB  :   float;
-                    AB  :   float;
-                    B_C :   float;
-                    }
+type cellPopulation = 
+                {   A : int<Types.cell>;
+                    B : int<Types.cell>;
+                    C : int<Types.cell>;  }
 
-type clone = {  numberA : int<Types.cell>;
-                numberB : int<Types.cell>;
-                numberC : int<Types.cell>;
+type clone = {  population  :   cellPopulation;
                 lambda  : float<Types.cell/Types.week>;
                 rho     : float;
                 r       : float;
@@ -16,30 +13,35 @@ type clone = {  numberA : int<Types.cell>;
                 time    : float<Types.week>;
                 rng     : System.Random;
                 reportFrequency  : float<Types.week>;
-                lastReport       : float<Types.week>;
+                lastReportTime   : float<Types.week>;
+                report  : cellPopulation option
                 }
                 with
                 member this.gamma = this.lambda * this.rho / (1. - this.rho)
-                member this.R = let R = ref None
-                                match !R with
-                                | Some(n) -> n
-                                | None ->   let n = this.lambda * float(this.numberA) * 1.<Types.cell> + this.gamma * float(this.numberB) * 1.<Types.cell>
-                                            R := Some(n)
-                                            n
-                member this.report = ()
-                member this.pAA =   this.r*(1.+this.delta)*this.lambda*float(this.numberA)*1.<Types.cell>/this.R
-                member this.pAB =   (1.-2.*this.r)*this.lambda*float(this.numberA)*1.<Types.cell>/this.R
-                member this.pBB =   this.r*(1.-this.delta)*this.lambda*float(this.numberA)*1.<Types.cell>/this.R
-                member this.pB2C =  float(this.numberB)*1.<Types.cell>*this.gamma/this.R
-                member this.update =    let dt = - 1.<Types.week> * log (this.rng.NextDouble()/ (this.R*1.<Types.week Types.cell^-2> ) )
+                member this.R = this.lambda * float(this.population.A) * 1.<Types.cell> + this.gamma * float(this.population.B) * 1.<Types.cell>
+                member this.pAA =   this.r*(1.+this.delta)*this.lambda*float(this.population.A)*1.<Types.cell>/this.R
+                member this.pAB =   (1.-2.*this.r)*this.lambda*float(this.population.A)*1.<Types.cell>/this.R
+                member this.pBB =   this.r*(1.-this.delta)*this.lambda*float(this.population.A)*1.<Types.cell>/this.R
+                member this.pB2C =  float(this.population.B)*1.<Types.cell>*this.gamma/this.R
+                member this.update =    //Update time, report value
+                                        let dt = - 1.<Types.week> * log (this.rng.NextDouble()/ (this.R*1.<Types.week Types.cell^-2> ) )
                                         let time'  = this.time + dt;
-                                        let (lastReport',ignore) = if time' - this.lastReport > this.reportFrequency then (time',this.report) else (this.lastReport,())
-                                        
-                                        {this with time = time'; lastReport = lastReport' }
+                                        let (lastReportTime',report') = if time' - this.lastReportTime > this.reportFrequency then (time',Some({A=this.population.A;B=this.population.B;C=this.population.C})) else (this.lastReportTime,None)
+                                        //Selection an action
+                                        let population'=    let random = this.rng.NextDouble()
+                                                            //AA
+                                                            if random < this.pAA then {this.population with A=this.population.A+1<Types.cell>}
+                                                            //AB
+                                                            else if random < this.pAA + this.pAB then {this.population with B=this.population.B+1<Types.cell>}
+                                                            //BB
+                                                            else if random < this.pAA + this.pAB + this.pBB then {this.population with A=this.population.A-1<Types.cell>;B=this.population.B+2<Types.cell>}
+                                                            //Migration
+                                                            else {this.population with B=this.population.B-1<Types.cell>;C=this.population.C+1<Types.cell>}
+                                        {this with population = population'; time = time'; lastReportTime = lastReportTime' ; report = report' }
 
-let initClone = {   numberA = 1<Types.cell>
-                    numberB = 0<Types.cell>
-                    numberC = 0<Types.cell>;
+let initClone = {   population = {  A = 1<Types.cell>
+                                    B = 0<Types.cell>
+                                    C = 0<Types.cell>; }
                     lambda  = 2.<Types.cell/Types.week>
                     rho = 0.85
                     r = 0.15
@@ -47,4 +49,13 @@ let initClone = {   numberA = 1<Types.cell>
                     time = 0.<Types.week>
                     rng = System.Random()
                     reportFrequency = 4.<Types.week>
-                    lastReport = 0.<Types.week> }
+                    lastReportTime = 0.<Types.week>
+                    report = None }
+
+let rec simulate clone timeLimit trace = 
+    match clone.time > timeLimit with
+    | true -> List.rev trace
+    | false ->  let clone' = clone.update
+                match clone'.report with
+                | None -> simulate clone' timeLimit trace
+                | Some(state) -> simulate clone' timeLimit (state::trace)
