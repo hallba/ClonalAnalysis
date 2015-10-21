cee9b2c16ea49b954b9905d142961f9110b315c5
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-10-21 15:18:53 +0100

    Tidy up and add reportStyle discriminated union

--- EpitheliumParameterSearch/SimulationCloneSizeDistribution.fs
+++ EpitheliumParameterSearch/SimulationCloneSizeDistribution.fs
@@ -10,6 +10,8 @@ type populationState =
                     time        : float<Types.week>;
                 }
 
+type reportStyle = Frequency of float<Types.week> | Specified of float<Types.week> list
+
 type clone = {  state   : populationState;
                 lambda  : float<Types.cell/Types.week>;
                 rho     : float;
@@ -28,6 +30,15 @@ type clone = {  state   : populationState;
                 member this.pAB =   (1.-2.*this.r)*this.lambda*float(this.state.population.A)*1.<Types.cell>/this.R
                 member this.pBB =   this.r*(1.-this.delta)*this.lambda*float(this.state.population.A)*1.<Types.cell>/this.R
                 member this.pB2C =  float(this.state.population.B)*1.<Types.cell>*this.gamma/this.R
+                member this.selectEvent =   let random = this.rng.NextDouble()
+                                            //AA
+                                            if random < this.pAA then {this.state.population with A=this.state.population.A+1<Types.cell>}
+                                            //AB
+                                            else if random < this.pAA + this.pAB then {this.state.population with B=this.state.population.B+1<Types.cell>}
+                                            //BB
+                                            else if random < this.pAA + this.pAB + this.pBB then {this.state.population with A=this.state.population.A-1<Types.cell>;B=this.state.population.B+2<Types.cell>}
+                                            //Migration
+                                            else {this.state.population with B=this.state.population.B-1<Types.cell>;C=this.state.population.C+1<Types.cell>}
                 member this.update =    //If the system has run out of stem cells, just update the final state and return the clone
                                         match this.finalState with
                                         | None ->
@@ -40,16 +51,7 @@ type clone = {  state   : populationState;
                                                                                         (reportTime,Some(state)) 
                                                                                     else (this.lastReportTime,None)
                                                     //Selection an action
-                                                    let population'=    let random = this.rng.NextDouble()
-                                                                        //AA
-                                                                        if random < this.pAA then {this.state.population with A=this.state.population.A+1<Types.cell>}
-                                                                        //AB
-                                                                        else if random < this.pAA + this.pAB then {this.state.population with B=this.state.population.B+1<Types.cell>}
-                                                                        //BB
-                                                                        else if random < this.pAA + this.pAB + this.pBB then {this.state.population with A=this.state.population.A-1<Types.cell>;B=this.state.population.B+2<Types.cell>}
-                                                                        //Migration
-                                                                        else {this.state.population with B=this.state.population.B-1<Types.cell>;C=this.state.population.C+1<Types.cell>}
-
+                                                    let population'= this.selectEvent
                                                     if population'.A + population'.B > 0<Types.cell> then {this with state = {population = population'; time = time'} ; lastReportTime = lastReportTime' ; report = report' }
                                                     else 
                                                         let finalReportTime = (time'-(time'%this.reportFrequency)) + this.reportFrequency
