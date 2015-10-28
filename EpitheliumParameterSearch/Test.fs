23730fc492e1f9f3300aea956d10d24cd3654089
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-10-28 12:01:32 +0000

    Manually entered testing dataset

--- EpitheliumParameterSearch/Test.fs
+++ EpitheliumParameterSearch/Test.fs
@@ -1,7 +1,9 @@
 ﻿module Test
 
+open CalculateLikelihood
+
 let kasumiParameters =  {
-                            Types.timePoints    =   [|(10.<Types.week>/7.); (11.<Types.week>/7.); (21.<Types.week>/7.) ; (42.<Types.week>/7.) ; (90.<Types.week>/7.) ; (180.<Types.week>/7.) ; (365.<Types.week>/7.); (545.<Types.week>/7.)|]
+                            Types.timePoints    =   [|((11.<Types.week>/7.); (21.<Types.week>/7.) ; (42.<Types.week>/7.) ; (90.<Types.week>/7.) ; (180.<Types.week>/7.) ; (365.<Types.week>/7.); (545.<Types.week>/7.)|]
                             Types.rhoRange      =   Array.init 46 (fun i -> float(i)*0.02 + 0.05 )
                             Types.rRange        =   Array.init 20 (fun i -> float(i)*0.025<Types.probability>  + 0.01<Types.probability>)
                             Types.lambdaRange   =   Array.init 50 (fun i -> float(i)*0.1<Types.cell/Types.week> + 0.1<Types.cell/Types.week>)
@@ -11,7 +13,7 @@ let kasumiParameters =  {
                             }
 
 let kasumiCG = {
-                            Types.timePoints    =   [|(10.<Types.week>/7.); (11.<Types.week>/7.); (21.<Types.week>/7.) ; (42.<Types.week>/7.) ; (90.<Types.week>/7.) ; (180.<Types.week>/7.) ; (365.<Types.week>/7.); (545.<Types.week>/7.)|]
+                            Types.timePoints    =   [|(11.<Types.week>/7.); (21.<Types.week>/7.) ; (42.<Types.week>/7.) ; (90.<Types.week>/7.) ; (180.<Types.week>/7.) ; (365.<Types.week>/7.); (545.<Types.week>/7.)|]
                             Types.rhoRange      =   Array.init 10 (fun i -> float(i)*0.1 + 0.05 )
                             Types.rRange        =   Array.init 4 (fun i -> float(i)*0.1<Types.probability>  + 0.05<Types.probability>)
                             Types.lambdaRange   =   Array.init 5 (fun i -> float(i)*1.0<Types.cell/Types.week> + 0.1<Types.cell/Types.week>)
@@ -21,3 +23,124 @@ let kasumiCG = {
                             }
 
 let testClone = Types.testSystem |> SimulationCloneSizeDistribution.parameterSetToClone (SimulationCloneSizeDistribution.Specified([1.428571429<Types.week>;])) ;;
+
+let kasumiWTData = [
+                    {
+                    time=(11.<Types.week>/7.)
+                    cloneSize=[|50;2|]
+                    }
+                    {
+                    time=(11.<Types.week>/7.)
+                    cloneSize=[|83;12;1;1|]
+                    }
+                    {
+                    time=(11.<Types.week>/7.)
+                    cloneSize=[|17;1|]
+                    }
+                    {
+                    time=(11.<Types.week>/7.)
+                    cloneSize=[|49;5;3|]
+                    }
+                    
+                    {
+                    time=(21.<Types.week>/7.)
+                    cloneSize=[|40;8;2;1;0;0;1|]
+                    }
+                    {
+                    time=(21.<Types.week>/7.)
+                    cloneSize=[|39;11;4|]
+                    }
+                    {
+                    time=(21.<Types.week>/7.)
+                    cloneSize=[|45;7|]
+                    }
+                    {
+                    time=(21.<Types.week>/7.)
+                    cloneSize=[|30;2;1|]
+                    }
+
+                    {
+                    time=(42.<Types.week>/7.)
+                    cloneSize=[|40;18;1;1;1|]
+                    }
+                    {
+                    time=(42.<Types.week>/7.)
+                    cloneSize=[|32;13;1;4;2;1|]
+                    }
+                    {
+                    time=(42.<Types.week>/7.)
+                    cloneSize=[|77;35;12;1;3;0;1|]
+                    }
+
+                    {
+                    time=(90.<Types.week>/7.)
+                    cloneSize=[|6;3;1;1;0;0;0;1|]
+                    }
+                    {
+                    time=(90.<Types.week>/7.)
+                    cloneSize=[|4;2;2;1;0;0;1|]
+                    }
+                    {
+                    time=(90.<Types.week>/7.)
+                    cloneSize=[|22;11;1;1;3;2|]
+                    }
+                    {
+                    time=(90.<Types.week>/7.)
+                    cloneSize=[|27;14;10;3;2;1;0;1;0;0;0;0;0;0;0;1;0;0;0;0;0;0;1|]
+                    }
+                    {
+                    time=(90.<Types.week>/7.)
+                    cloneSize=[|35;23;11;6;3;1;2;0;1|]
+                    }
+
+                    {
+                    time=(180.<Types.week>/7.)
+                    cloneSize=[|16;6;1;1;0;0;1;1;0;1;0;0;1|]
+                    }
+                    {
+                    time=(180.<Types.week>/7.)
+                    cloneSize=[|13;10;4;5;2;0;2;1;1;1|]
+                    }
+                    {
+                    time=(180.<Types.week>/7.)
+                    cloneSize=[|25;7;2;2;4;0;1;5;0;1;0;0;1;0;0;1;0;0;0;1;0;1|]
+                    }
+                    {
+                    time=(180.<Types.week>/7.)
+                    cloneSize=[|29;19;10;6;1;2;2;1;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1|]
+                    }
+                    {
+                    time=(180.<Types.week>/7.)
+                    cloneSize=[|24;12;7;0;2;2;0;1;0;0;0;0;0;0;0;0;0;0;0;1|]
+                    }
+                    
+                    {
+                    time=(365.<Types.week>/7.)
+                    cloneSize=[|29;17;11;9;4;0;1;0;1;0;1;2;1;1;0;0;0;0;0;0;0;0;0;0;0;0;3;1|]
+                    }
+                    {
+                    time=(365.<Types.week>/7.)
+                    cloneSize=[|22;14;9;5;6;5;0;5;1;1;1;3;1;0;0;3;1;0;0;1;0;0;0;0;0;1;0;1;0;1;0;0;0;0;0;0;0;0;0;0;1|]
+                    }
+                    {
+                    time=(365.<Types.week>/7.)
+                    cloneSize=[|25;15;9;3;5;3;5;0;0;3;2;1;0;1;1;0;2;0;0;0;0;1;0;0;0;0;0;0;0;1;0;0;0;0;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1|]
+                    }
+                    {
+                    time=(365.<Types.week>/7.)
+                    cloneSize=[|28;5;5;7;1;0;0;1;1;1;1;0;1;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1|]
+                    }
+
+                    {
+                    time=(545.<Types.week>/7.)
+                    cloneSize=[|31;10;7;5;3;5;1;2;0;0;3;1;0;0;1;1;0;1;0;0;0;0;1;0;1;0;0;0;0;1;0;0;0;0;2;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;1|]
+                    }
+                    {
+                    time=(545.<Types.week>/7.)
+                    cloneSize=[|23;9;9;4;2;1;2;1;2;1;2;2;1;1;0;0;0;1;0;1;0;0;0;0;0;0;0;0;0;0;1;1;0;0;1;0;0;0;0;0;1;0;0;1;1|]
+                    }
+                    {
+                    time=(545.<Types.week>/7.)
+                    cloneSize=[|6;6;2;4;0;0;0;0;1;0;1;0;0;1;0;0;0;0;1|]
+                    }
+                    ]
