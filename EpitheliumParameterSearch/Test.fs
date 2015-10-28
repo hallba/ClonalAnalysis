a075b6df7abb04ae6b8495f443a56f5584fd5d4f
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-10-28 23:24:24 +0000

    Added directly comparable parameters to kasumiCG =~ 4 hours compute by simulation

--- EpitheliumParameterSearch/Test.fs
+++ EpitheliumParameterSearch/Test.fs
@@ -14,13 +14,13 @@ let kasumiParameters =  {
 
 let kasumiCG = {
                             Types.timePoints    =   [|(11.<Types.week>/7.); (21.<Types.week>/7.) ; (42.<Types.week>/7.) ; (90.<Types.week>/7.) ; (180.<Types.week>/7.) ; (365.<Types.week>/7.); (545.<Types.week>/7.)|]
-                            Types.rhoRange      =   Array.init 10 (fun i -> float(i)*0.1 + 0.05 )
-                            Types.rRange        =   Array.init 4 (fun i -> float(i)*0.1<Types.probability>  + 0.05<Types.probability>)
-                            Types.lambdaRange   =   Array.init 5 (fun i -> float(i)*1.0<Types.cell/Types.week> + 0.1<Types.cell/Types.week>)
+                            Types.rhoRange      =   Array.init 15 (fun i -> float(i)*0.05 + 0.15 )
+                            Types.rRange        =   Array.init 9 (fun i -> float(i)*0.05<Types.probability>  + 0.1<Types.probability>)
+                            Types.lambdaRange   =   Array.init 21 (fun i -> float(i)*0.2<Types.cell/Types.week> + 1.<Types.cell/Types.week>)
                             Types.maxN          =   195
                             Types.deltaRange    =   Types.Zero
                             Types.results       =   None
-                            }
+                            }//200 = 20 mins Simulation
 
 let testClone = Types.testSystem |> SimulationCloneSizeDistribution.parameterSetToClone (SimulationCloneSizeDistribution.Specified([1.428571429<Types.week>;])) ;;
 
