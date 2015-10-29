efb771f8fe60450b18e0447dbdf4ee42656e8c97
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-10-29 23:16:02 +0000

    Code for more efficiently wrapping parallel commands

---
+++ EpitheliumParameterSearch/Parallel.fs
@@ -0,0 +1,16 @@
+﻿module Parallel
+
+let threads = System.Environment.ProcessorCount *2
+
+let arrayMap f c =
+    //Array.Parallel.map f c
+    let tasks = Array.length c
+    let tasksPerThread = tasks/threads
+    let additionalTasks = tasks%threads
+    let pstruct =   Array.init threads (fun i ->    let threadTasks = if i=(threads-1) then tasksPerThread + additionalTasks else tasksPerThread
+                                                    Array.init threadTasks (fun j -> c.[i*tasksPerThread+j] )
+                                                    )
+                    |> Array.Parallel.map (fun taskBundle -> Array.map f taskBundle )
+    //Unbundle and return array
+    Array.init tasks (fun taskID -> pstruct.[(max (tasks/taskID) (threads-1) )].[dummy] )
+    
