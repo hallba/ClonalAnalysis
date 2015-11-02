4200e743797a0233e71121dd62aefb958734c57e
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2015-11-02 09:06:31 +0000

    Another failing test- this one is a problem due to the precision of whittaker functions

--- EpitheliumParameterSearch/Test.fs
+++ EpitheliumParameterSearch/Test.fs
@@ -3,3 +3,9 @@
 open MathNet.Numerics
 
 ignore (Kummer.M (complex 0.193813772152103 0.) (complex 0.999 0.) (complex 33.8845488347767 -10.6505935688991))
+
+
+let gamma = 0.05263157895
+let T =  0.1571428571<Types.cell>
+let k_50 = (complex -0.0320515775716552 0.999486216200688)
+ignore (AnalyticalCloneSizeDistribution.F k_50 k_50 (T*1.<Types.cell^-1>) 0.01 gamma)
