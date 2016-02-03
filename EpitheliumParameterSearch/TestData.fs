1fd067c8de87efc7ee6c6be375e021f751872d6c
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-02-03 12:50:51 +0000

    Delta scan for K p53 data added to test set

--- EpitheliumParameterSearch/TestData.fs
+++ EpitheliumParameterSearch/TestData.fs
@@ -13,6 +13,17 @@ let kasumiParameters =  {
                             Types.excludeOnes   =   true
                             }
 
+let kasumip53 =  {
+                            Types.timePoints    =   [|(11.<Types.week>/7.); (21.<Types.week>/7.) ; (42.<Types.week>/7.) ; (90.<Types.week>/7.) ; (180.<Types.week>/7.) ; (365.<Types.week>/7.); (545.<Types.week>/7.)|]
+                            Types.rhoRange      =   [|0.6;|]
+                            Types.rRange        =   [|0.01<Types.probability>;|]
+                            Types.lambdaRange   =   [|1.2<Types.cell/Types.week>;|]
+                            Types.maxN          =   195
+                            Types.deltaRange    =   Types.Range(List.init 100 (fun i -> float(i)*0.01<Types.probability>) )
+                            Types.results       =   None
+                            Types.excludeOnes   =   true
+                            }
+
 let kasumiCG = {
                             Types.timePoints    =   [|(11.<Types.week>/7.); (21.<Types.week>/7.) ; (42.<Types.week>/7.) ; (90.<Types.week>/7.) ; (180.<Types.week>/7.) ; (365.<Types.week>/7.); (545.<Types.week>/7.)|]
                             Types.rhoRange      =   Array.init 15 (fun i -> float(i)*0.05 + 0.15 )
