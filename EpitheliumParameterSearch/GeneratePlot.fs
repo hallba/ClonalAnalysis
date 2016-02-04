069610e61cc1ca35734634c08b53171fca4e283d
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-02-04 14:17:22 +0000

    Function for generating an Xfarbe input file from a 2D array

---
+++ EpitheliumParameterSearch/GeneratePlot.fs
@@ -0,0 +1,31 @@
+﻿module GeneratePlot
+
+open System
+open System.Collections.Generic
+open System.IO
+
+let twoDArrayToString matrix =
+    let xL = Array.length matrix
+    let yL = Array.length matrix.[0]
+    let mutable s = ""
+    for i=0 to (xL - 1) do
+        for j=0 to (yL - 1) do
+            s <- s + (sprintf " %A" matrix.[i].[j])
+        s <- s + "\n"
+    s
+
+let twoDArrayToXfarbe matrix filename (title: string option) = 
+    use file = new StreamWriter(filename, false)
+    let xL = Array.length matrix
+    let yL = Array.length matrix.[0]
+    ignore (match title with
+            | None -> ()
+            | Some(s) -> file.WriteLine(s)
+            )
+    file.WriteLine(sprintf "%A %A" xL yL)
+    file.WriteLine(twoDArrayToString matrix)
+    file.Close
+
+
+
+
