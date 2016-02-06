8a1e8b27dec778490647a5646281b66675476231
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-02-06 20:33:32 +0000

    File to allow binary storage of performed searches

---
+++ EpitheliumParameterSearch/IO.fs
@@ -0,0 +1,17 @@
+﻿module IO
+
+let storeSearch (filename: string) (search: Types.parameterSearch) = 
+        use file = new System.IO.FileStream(filename, System.IO.FileMode.Create)
+        let bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter()
+        use mem = new System.IO.MemoryStream()
+        bf.Serialize(mem,search)
+        let byteArray = mem.ToArray()
+        file.Write(byteArray,0,byteArray.Length)
+        file.Close()
+        ()
+
+let retrieveSearch (filename: string) =
+    use fileStream = new System.IO.FileStream(filename, System.IO.FileMode.Open)
+    let bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter()
+    let result = bf.Deserialize(fileStream)
+    (result :?> Types.parameterSearch)
