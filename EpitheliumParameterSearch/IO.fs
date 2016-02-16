169f0ade41ed62ec595cab7d7a6a3893e2fe99d6
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-02-16 14:40:52 +0000

    Currently reads in all data from matlab matrix

--- EpitheliumParameterSearch/IO.fs
+++ EpitheliumParameterSearch/IO.fs
@@ -1,8 +1,5 @@
 ﻿module IO
 
-open MathNet.Numerics.LinearAlgebra
-open MathNet.Numerics.Data.Matlab
-
 let storeSearch (filename: string) (search: Types.parameterSearch) = 
         use file = new System.IO.FileStream(filename, System.IO.FileMode.Create)
         let bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter()
@@ -22,26 +19,44 @@ let retrieveSearch (filename: string) =
 
 //Get data from a matlab generated matrix
 let importMatlab (filename: string) = 
+
+    let m = new csmatio.io.MatFileReader(filename)
     
-    let matrixToArray1D (m:Matrix<float>) = 
-        let l = Matrix.columnCount m
-        Array.init l (fun i -> m.[0,i])
+    let read1d name =
+        let r = m.Content.[name] :?> csmatio.types.MLDouble
+        let s = r.Dimensions
+        let i = r.GetArray()
+        i.[0]
 
-    let matrixToArray4D (m:Matrix<float>) = 
-        ()
+    //readNd - Read a multidimensional matrix from an open matlab file and return an array of arrays (of arrays of arrays...)
+    let readNd name (dimensions:int list) = 
+        let rec core d acc =
+            match d with
+            | [] -> acc
+            | topDimension::rest -> core rest acc
 
-    let m = (MatlabReader.ReadAll<float>(filename,"rhoRange","lambdaRange","rRange","PScanPP","PSurvScanPP","t","tExp","timePoints") ) // :?> System.Collections.Generic.Dictionary< string, Matrix<float> > 
+        let r = m.Content.[name] :?> csmatio.types.MLDouble
+
+        //First- read all of the values into a 1D array
+        let rec totalElements d acc = 
+            match d with 
+            | [] -> acc
+            | topD::rest -> totalElements rest (acc*topD)
+        
+        Array.init (totalElements dimensions 1) (fun i -> r.Get(i))
+
+    let rRange = read1d "rRange"
+    let lambdaRange = read1d "lambdaRange"
+    let rhoRange = read1d "rhoRange" |> Array.map (fun i -> 1. - i) //Rho correction due to gamma inversion in matlab code
+    let t = read1d "t"
+    let timePoints = read1d "timePoints"
+    let nRange = read1d "nRange"
+
+    //could also read maxN and nBadvalues
+
+    let cloneSizeP = readNd "PScanPP" ([lambdaRange;rhoRange;rRange;t;nRange] |> List.map (fun i -> Array.length i))
+    let survivalP  = readNd "PSurvScanPP" ([lambdaRange;rhoRange;rRange;t] |> List.map (fun i -> Array.length i))
+
     
-    let rRange = m.["rRange"] |> matrixToArray1D
-    let lambdaRange = m.["lambdaRange"] |> matrixToArray1D
-    let rhoRange = m.["rhoRange"] |> matrixToArray1D
-    let t = m.["t"] |> matrixToArray1D
-    let tExp = m.["tExp"] |> matrixToArray1D
-    let timePoints = m.["timePoints"] |> matrixToArray1D
-    
-    let l = MatlabReader.List(filename)
 
-    //This will never work because MathNet matrices are 2D :(
-
-
-    m
+    rRange
