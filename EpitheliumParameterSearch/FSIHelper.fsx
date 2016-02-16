3111884df0b830d9774b8de9bd6b3688d726f0b8
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-02-16 10:31:36 +0000

    Helper script for FSI

---
+++ EpitheliumParameterSearch/FSIHelper.fsx
@@ -0,0 +1,14 @@
+﻿#load "Gamma.fs"
+#load "Kummer.fs"
+#load "Whittaker.fs"
+#load "Parallel.fs"
+#load "Types.fs"
+#load "SimulationCloneSizeDistribution.fs"
+#load "AnalyticalCloneSizeDistribution.fs"
+#load "CalculateProbability.fs"
+#load "CalculateLikelihood.fs"
+#load "TestData.fs"
+
+let sim = CalculateProbability.parameterSearch TestData.kasumip53 CalculateProbability.Simulation
+
+let L = CalculateLikelihood.getLikelihood TestData.kasumip53Data sim
