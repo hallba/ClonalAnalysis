cd5ce2a2a1766a20054a150490c86d8652b48193
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-02-16 18:38:18 +0000

    Now reads cells properly. Still needs clone size problem solved

--- EpitheliumParameterSearch/IO.fs
+++ EpitheliumParameterSearch/IO.fs
@@ -30,11 +30,6 @@ let importMatlab (filename: string) =
 
     //readNd - Read a multidimensional matrix from an open matlab file and return an array of arrays (of arrays of arrays...)
     let readNd name (dimensions:int list) = 
-        let rec core d acc =
-            match d with
-            | [] -> acc
-            | topDimension::rest -> core rest acc
-
         let r = m.Content.[name] :?> csmatio.types.MLDouble
 
         //First- read all of the values into a 1D array
@@ -45,18 +40,43 @@ let importMatlab (filename: string) =
         
         Array.init (totalElements dimensions 1) (fun i -> r.Get(i))
 
-    let rRange = read1d "rRange"
-    let lambdaRange = read1d "lambdaRange"
+    let readCells name = 
+        let r = m.Content.[name] :?> csmatio.types.MLCell
+        Array.init r.Size (fun i -> (r.Cells.[i] :?> csmatio.types.MLDouble).GetArray())
+
+    let rRange = read1d "rRange" |> Array.map (fun i -> i*1.<Types.probability>)
+    let lambdaRange = read1d "lambdaRange" |> Array.map (fun i -> i*1.<Types.cell/Types.week>)
     let rhoRange = read1d "rhoRange" |> Array.map (fun i -> 1. - i) //Rho correction due to gamma inversion in matlab code
-    let t = read1d "t"
+    let t = read1d "t" |> Array.map (fun i -> i*1.<Types.week>)
     let timePoints = read1d "timePoints"
-    let nRange = read1d "nRange"
+    let nRange = readCells "nRange"
+    let maxN = int (read1d "maxN").[0]
+
+    let input = {   Types.rhoRange=rhoRange
+                    Types.rRange=rRange
+                    Types.lambdaRange=lambdaRange
+                    Types.deltaRange=Types.Zero
+                    Types.timePoints=t
+                    Types.maxN=maxN
+                    Types.results=None
+                    Types.excludeOnes=true
+                    }
+
+    let cloneSizeP = readNd "PScanPP"  [(Array.length lambdaRange);(Array.length rhoRange);(Array.length rRange);(Array.length t)] //This is a problem- n is of variable length
+    let survivalP  = readNd "PSurvScanPP" [(Array.length lambdaRange);(Array.length rhoRange);(Array.length rRange);(Array.length t)]
+
+    Types.restructureParameterSet input survivalP cloneSizeP
 
     //could also read maxN and nBadvalues
 
-    let cloneSizeP = readNd "PScanPP" ([lambdaRange;rhoRange;rRange;t;nRange] |> List.map (fun i -> Array.length i))
-    let survivalP  = readNd "PSurvScanPP" ([lambdaRange;rhoRange;rRange;t] |> List.map (fun i -> Array.length i))
-
-    
-
-    rRange
+//    let scanMatD = ([lambdaRange;rhoRange;rRange;t;nRange] |> List.map (fun i -> Array.length i))
+//    let cloneSizeP = readNd "PScanPP" scanMatD
+//                     |> fun matrix ->
+//                        Array.init scanMatD.[0] 
+//                            (fun i -> (Array.init scanMatD.[1] 
+//                                (fun j -> (Array.init scanMatD.[2] 
+//                                    (fun k -> ( Array.init scanMatD.[3]
+//                                        (fun l -> ( Array.init scanMatD.[4] 
+//                                            (fun m -> matrix.[i+(j*scanMatD.[0])+(k*scanMatD.[0]*scanMatD.[1])+(l*scanMatD.[0]*scanMatD.[1]*scanMatD.[2])+(m*scanMatD.[0]*scanMatD.[1]*scanMatD.[2]*scanMatD.[3])] 
+//                                                ) ) ) ) ) ) ) ) )
+//    let survivalP  = readNd "PSurvScanPP" ([lambdaRange;rhoRange;rRange;t] |> List.map (fun i -> Array.length i))
