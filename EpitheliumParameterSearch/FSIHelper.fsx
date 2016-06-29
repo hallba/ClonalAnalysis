4517b2b6156af06a2b734b466211f1c0bc307173
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-06-29 13:40:21 +0100

    Notes on how to run histone dilution script

--- EpitheliumParameterSearch/FSIHelper.fsx
+++ EpitheliumParameterSearch/FSIHelper.fsx
@@ -5,6 +5,8 @@
 #load "Parallel.fs"
 #load "Types.fs"
 #load "IO.fs"
+#load "HistoneDilution.fs"
+#load "SupraBasalAnalysis.fs"
 #load "SimulationCloneSizeDistribution.fs"
 #load "AnalyticalCloneSizeDistribution.fs"
 #load "CalculateProbability.fs"
@@ -17,4 +19,13 @@ let sim = CalculateProbability.parameterSearch TestData.kasumip53 CalculateProba
 
 let L = CalculateLikelihood.getLikelihood TestData.kasumip53Data sim
 
+open SimulationCloneSizeDistribution
+
 let c = {initClone with r=0.25;rho=0.5;reporting=Regular{timeLimit=4.<Types.week>;frequency=1.<Types.week>;lastReport=0.<Types.week>}}
+let c2 = {c with lambda=c.lambda/3.}
+
+let la = HistoneDilution.dilutionSinglePopulation 1000000 c |> List.map (fun i -> List.map (fun j -> log(j)) i )
+let lb = HistoneDilution.dilutionTwoPopulation 660000 c 340000 c2|> List.map (fun i -> List.map (fun j -> log(j)) i )
+
+HistoneDilution.intensityToText la "pure.dat"
+HistoneDilution.intensityToText lb "mixed.dat"
