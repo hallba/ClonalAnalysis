a0484535937b3f576381035091915ab34c33f840
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-10-04 19:46:57 +0100

    File changes and more lines in the FSI helper script

--- EpitheliumParameterSearch/FSIHelper.fsx
+++ EpitheliumParameterSearch/FSIHelper.fsx
@@ -5,9 +5,9 @@
 #load "Parallel.fs"
 #load "Types.fs"
 #load "IO.fs"
+#load "SimulationCloneSizeDistribution.fs"
 #load "HistoneDilution.fs"
 #load "SupraBasalAnalysis.fs"
-#load "SimulationCloneSizeDistribution.fs"
 #load "AnalyticalCloneSizeDistribution.fs"
 #load "CalculateProbability.fs"
 #load "CalculateLikelihood.fs"
@@ -21,11 +21,22 @@ let L = CalculateLikelihood.getLikelihood TestData.kasumip53Data sim
 
 open SimulationCloneSizeDistribution
 
-let c = {initClone with r=0.25;rho=0.5;reporting=Regular{timeLimit=4.<Types.week>;frequency=1.<Types.week>;lastReport=0.<Types.week>}}
+let c = {initClone with 
+            r=0.25;
+            rho=0.5;
+            state={initClone.state with population={initClone.state.population with dilution=Population([|1.|])}}
+            reporting=Regular{timeLimit=4.<Types.week>;frequency=1.<Types.week>;
+            lastReport=0.<Types.week>}}
+
 let c2 = {c with lambda=c.lambda/3.}
 
-let la = HistoneDilution.dilutionSinglePopulation 1000000 c |> List.map (fun i -> List.map (fun j -> log(j)) i )
-let lb = HistoneDilution.dilutionTwoPopulation 660000 c 340000 c2|> List.map (fun i -> List.map (fun j -> log(j)) i )
+let log2 = log(2.)
+
+let la = HistoneDilution.dilutionSinglePopulation 1000000 c |> List.map (fun i -> List.map (fun j -> log(j)/log2) i )
+let la2 = HistoneDilution.dilutionSinglePopulation 1000000 c2 |> List.map (fun i -> List.map (fun j -> log(j)/log2) i )
+
+let lb = HistoneDilution.dilutionTwoPopulation 660000 c 340000 c2|> List.map (fun i -> List.map (fun j -> log(j)/log2) i )
 
 HistoneDilution.intensityToText la "pure.dat"
+HistoneDilution.intensityToText la2 "pure2.dat"
 HistoneDilution.intensityToText lb "mixed.dat"
