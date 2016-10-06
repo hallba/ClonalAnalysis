ae217aa7185be2d71c4aa8e77c49de42b4f5e449
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-10-06 11:32:19 +0100

    Fixed unit test

--- EpitheliumParameterSearch/UnitTests.fs
+++ EpitheliumParameterSearch/UnitTests.fs
@@ -2,7 +2,27 @@
 
 open SimulationCloneSizeDistribution
 
+type TestResult = Pass of populationState list| Fail of populationState list
+
+let floatRatio a b n = 
+    //Convienience function for calculating deviation of a ratios of ints (a,b) compared to another float (n)
+    (float(a)/float(b))/n
+    
 //Homeostasis
+let testHomeoState (c:clone) (p:populationState) =
+    let sbH =   match c.SBRatio with 
+                | None -> None
+                | Some(n) ->    let actualRatio = floatRatio p.population.suprabasal p.population.basal n
+                                if actualRatio <1.1 && actualRatio > 0.9 then None else Some(n,p.population.suprabasal,p.population.basal,(floatRatio p.population.suprabasal p.population.basal n))
+    let actualRho = floatRatio p.population.A p.population.B (c.rho/(1.-c.rho))  
+    let rhoH =  if actualRho <1.1 && actualRho > 0.9 then None else Some((c.rho/(1.-c.rho)),p.population.A,p.population.B,(floatRatio p.population.A p.population.B (c.rho/(1.-c.rho))) )
+    (sbH,rhoH)
+
+let testHomeostasis (result:populationState list) (c:clone) =
+    List.map (testHomeoState c) result 
+
+let parseHStasisResult i =  fst(i) <> None && snd(i) <> None
+
 let homeostasis_rho50_r25 =
     let tissueSize = {initClone.state.population with A=1000<Types.cell>;B=1000<Types.cell>;C=2000<Types.cell>}
     let tissue = {initClone with    state={initClone.state with population=tissueSize};
@@ -14,10 +34,12 @@ let homeostasis_rho50_r25 =
 
     let result = simulate tissue
     //Need a function here to test that result- should have A~B C~A+B
-    result
+    testHomeostasis result tissue 
+    |> List.filter parseHStasisResult
+    |> (fun i -> if List.length i > 0 then Fail(result) else Pass(result))
 
 let homeostasis_rho65_r25 = 
-    let tissueSize = {initClone.state.population with A=1400<Types.cell>;B=600<Types.cell>;C=2000<Types.cell>}
+    let tissueSize = {initClone.state.population with A=1300<Types.cell>;B=700<Types.cell>;C=2000<Types.cell>}
     let tissue = {initClone with    state={initClone.state with population=tissueSize};
                                     rho=0.65;
                                     SBRatio=Some(1.);
@@ -25,4 +47,6 @@ let homeostasis_rho65_r25 =
                                     lambda=2.<Types.cell/Types.week>
                                     }
     let result = simulate tissue
-    result
+    testHomeostasis result tissue
+    |> List.filter parseHStasisResult
+    |> (fun i -> if List.length i > 0 then Fail(result) else Pass(result))
